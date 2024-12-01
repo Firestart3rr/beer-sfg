f60ed6186ca9f8541f7d1cffc3acaca18092c0cf
Jakub Wziatka <kuba.wziatka@gmail.com>
2024-12-01 15:18:02 +0100

    Database Constraint Validation

--- src/test/java/guru/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
+++ src/test/java/guru/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
@@ -2,6 +2,7 @@ package guru.springframework.spring6restmvc.repositories;
 
 import guru.springframework.spring6restmvc.entities.Beer;
 import guru.springframework.spring6restmvc.model.BeerStyle;
+import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -9,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DataJpaTest
 class BeerRepositoryTest {
@@ -17,17 +19,17 @@ class BeerRepositoryTest {
     BeerRepository beerRepository;
 
     @Test
-    void testSaveBeer() {
-        Beer savedBeer = beerRepository.save(Beer.builder()
-                .beerName("My Beer")
-                .beerStyle(BeerStyle.PALE_ALE)
-                .upc("23423423423432")
-                .price(new BigDecimal("11.99"))
-                .build());
+    void testSaveBeerNameTooLong() {
 
-        beerRepository.flush();
+        assertThrows(ConstraintViolationException.class, () -> {
+            Beer savedBeer = beerRepository.save(Beer.builder()
+                    .beerName("My BeerMy BeerMy BeerMy BeerMy BeerMy BeerMy BeerMy BeerMy BeerMy BeerMy BeerMy BeerMy BeerMy BeerMy BeerMy BeerMy Beer")
+                    .beerStyle(BeerStyle.PALE_ALE)
+                    .upc("23423423423432")
+                    .price(new BigDecimal("11.99"))
+                    .build());
 
-        assertThat(savedBeer).isNotNull();
-        assertThat(savedBeer.getId()).isNotNull();
+            beerRepository.flush();
+        });
     }
 }
